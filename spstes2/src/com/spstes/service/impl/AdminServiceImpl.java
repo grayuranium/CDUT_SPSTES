ff095804df4897998e5c01faf0fe68feff0f2124
grayuranium <sjh578258779@qq.com>
2018-08-03 09:14:44 +0800

    不好意思这才是学生界面和考试院框架

---
+++ spstes2/src/com/spstes/service/impl/AdminServiceImpl.java
@@ -0,0 +1,10 @@
+package com.spstes.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.spstes.service.AdminService;
+
+@Service
+public class AdminServiceImpl implements AdminService {
+
+}
---
+++ spstes2/src/com/spstes/service/impl/StudentServiceImpl.java
@@ -0,0 +1,19 @@
+package com.spstes.service.impl;
+
+import com.spstes.service.StudentService;
+
+public class StudentServiceImpl implements StudentService {
+
+	@Override
+	public void addExam() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void searchInfo(Object obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
